29e25f717a574f6f8dbeadc6aaf19c4667c4e14e
Yaping Xin <sanduniyasodha@gmail.com>
2018-11-23 01:09:07 +0530

    create dashbord ui-sanduni

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts.app')
-
+<style>
+    .col-xs-6 {
+        background-color: lightgrey;
+        width: 500px;
+        border: 25px solid green;
+        padding: 25px;
+        margin: 25px;
+    }
+    </style>
 @section('content')
+
+<div class="w3-sidebar w3-bar-block w3-light-grey w3-card" style="width:20%">
+    <a href="http://localhost:8000/assign" class="w3-bar-item w3-button">assign</a>
+    <a href="http://localhost:8000/meeting" class="w3-bar-item w3-button">meeting</a>
+    {{-- <div class="w3-dropdown-hover">
+      <button class="w3-button">Dropdown
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="w3-dropdown-content w3-bar-block">
+        <a href="#" class="w3-bar-item w3-button">Link</a>
+        <a href="#" class="w3-bar-item w3-button">Link</a>
+      </div>
+    </div> --}}
+    <a href="http://localhost:8000/createUser" class="w3-bar-item w3-button">create user</a>
+    <a href="http://localhost:8000/event" class="w3-bar-item w3-button">event</a>
+  </div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -16,12 +40,7 @@
 
                     You are logged in...!!<br><br>
 
-                   <a href="http://localhost:8000/event">event</a><br>
-                   <a href="http://localhost:8000/meeting">meeting</a><br>
 
-                   <a href="http://localhost:8000/createUser">create user</a><br>
-
-                   <a href="http://localhost:8000/assign">assign</a><br>
 
                    @role('or_fol')
                    <p> orfol</p>
@@ -37,9 +56,45 @@
                    @endrole
 
 
+                    <h2>Welcome</h2>
+                    <h3>Upcoming Events</h3>
+
+                    <div class="col-sm-12">
+                        <div class="raw">
+                            <div class="col-xs-6">
+                                <h3>Event : donate the play ground<h3><hr>
+                                <ul>
+                                    <li>Region : Godagama, Panadura</li>
+                                    <li>Start date : 2016/01/07</li>
+                                    <li>Start time : 08:30 AM </li>
+                                    <li>Budget : Rs.200,000 </li>
+                                    <li>Reason :No any other ground of this region and it has lot of good players, but they cannot practice well without ground</li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-6">
+                                <h3>Event : repair the bridge<h3><hr>
+                                <ul>
+                                    <li>Region : yatiyana, ampara</li>
+                                    <li>Start date : 2017/02/17</li>
+                                    <li>Start time : 10:30 AM </li>
+                                    <li>Budget : Rs.350,000 </li>
+                                    <li>Reason :No any bridge connect to the five villages through river. Existing bridge had very dangerous situation.because it not repair since 10 years.</li>
+                                </ul>
+                            </div>
+                         </div>
+                    </div>
+
+                    <h3>Upcoming Meetings</h3>
+
+
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
+
+
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,18 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
- 
+
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    
+
     <!-- J query -->
 
 
-    
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
@@ -24,10 +27,10 @@
     <script
   integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
   crossorigin="anonymous"></script>
-  
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
- 
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -43,7 +46,7 @@
 
 </head>
 <body>
-    
+
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
