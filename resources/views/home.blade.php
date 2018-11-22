3d29b698abc6de901a6cb9a6be3d97e30058c6a4
Yaping Xin <sanduniyasodha@gmail.com>
2018-11-23 01:14:58 +0530

    create dashbord ui-sanduni

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,6 @@
 @extends('layouts.app')
-<style>
-    .col-xs-6 {
-        background-color: lightgrey;
-        width: 500px;
-        border: 25px solid green;
-        padding: 25px;
-        margin: 25px;
-    }
-    </style>
+
+
 @section('content')
 
 <div class="w3-sidebar w3-bar-block w3-light-grey w3-card" style="width:20%">
@@ -56,35 +49,11 @@
                    @endrole
 
 
-                    <h2>Welcome</h2>
-                    <h3>Upcoming Events</h3>
+                    <h2>Welcome</h2><br>
+                    <h4>Upcoming Events</h4><hr>
 
-                    <div class="col-sm-12">
-                        <div class="raw">
-                            <div class="col-xs-6">
-                                <h3>Event : donate the play ground<h3><hr>
-                                <ul>
-                                    <li>Region : Godagama, Panadura</li>
-                                    <li>Start date : 2016/01/07</li>
-                                    <li>Start time : 08:30 AM </li>
-                                    <li>Budget : Rs.200,000 </li>
-                                    <li>Reason :No any other ground of this region and it has lot of good players, but they cannot practice well without ground</li>
-                                </ul>
-                            </div>
-                            <div class="col-xs-6">
-                                <h3>Event : repair the bridge<h3><hr>
-                                <ul>
-                                    <li>Region : yatiyana, ampara</li>
-                                    <li>Start date : 2017/02/17</li>
-                                    <li>Start time : 10:30 AM </li>
-                                    <li>Budget : Rs.350,000 </li>
-                                    <li>Reason :No any bridge connect to the five villages through river. Existing bridge had very dangerous situation.because it not repair since 10 years.</li>
-                                </ul>
-                            </div>
-                         </div>
-                    </div>
-
-                    <h3>Upcoming Meetings</h3>
+                    <br><br>
+                    <h4>Upcoming Meetings</h4><hr>
 
 
                 </div>
