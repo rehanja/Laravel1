4ffa7c831c3649bc2f78f3f083fc9a865db36e2d
Yaping Xin <sanduniyasodha@gmail.com>
2019-01-23 10:20:51 +0530

    add assign or-fol functionality

--- config/app.php
+++ config/app.php
@@ -151,6 +151,8 @@ return [
          * Package Service Providers...
          */
 
+        Collective\Html\HtmlServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -159,6 +161,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
 
     ],
 
@@ -209,6 +212,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
--- resources/views/assign/assignOrFol.blade.php
+++ resources/views/assign/assignOrFol.blade.php
@@ -3,10 +3,24 @@
 
 @section('content')
 
-<div class="container">
+<div class="container text-center">
+
+    <h2>Assign OR-Fol</h2>
+
     <div id="map">
 
     </div>
+    <br>
+    
+    {!! Form::open([]) !!}
+
+    {!! Form::label('district','District') !!}
+
+    {!! Form::select('district',['L' => 'Large', 'S' => 'Small']) !!}
+
+    {!! Form::close() !!}
+
+
 </div>
 
 
